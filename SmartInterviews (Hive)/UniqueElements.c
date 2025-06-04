e24e28bb0a797aa3201d5b7d2f47ef0e3bbd58fe
Uma Iyer <152464173+uma-iyer-24@users.noreply.github.com>
2025-06-04 16:35:24 +0530

    Update UniqueElements.c

--- SmartInterviews (Hive)/UniqueElements.c
+++ SmartInterviews (Hive)/UniqueElements.c
@@ -30,35 +30,48 @@ Self Explanatory
 
 int main() {
 
-    int n, flag = 0;
+    int n, x, flag, k;
     scanf("%d", &n);
-    int a[n];
+    int arr[n], unique[n], freq[n], index = 0;
 
+    //reading input and initializing arrays
     for(int i = 0; i < n; i++)
     {
-        scanf("%d", &a[i]);
+        scanf("%d", &arr[i]);
+        unique[i] = -1;
+        freq[i] = 0;
     }
 
+    //checking for uniqueness before inserting into unique elements array
+    //creating freq table for unique array
     for(int i = 0; i < n; i++)
     {
+        flag = 0;
+        for(int j = 0; j < n; j++)
+            if(arr[i] == unique[j])
+                flag = 1;
 
-    if(a[i] == -1)
-        continue;
 
-        for(int j = i + 1; j < n && i != j; j++)
+        if(flag == 0)
+            unique[index++] = arr[i];
+    }
+
+    //THIS TOOK HALF AN HOUR TO FIGURE OUT
+    for(int i = 0; i < index; i++)
+    {
+        for(int j = 0; j < n; j++)
         {
-            if(a[i] == a[j])
-            {
-                a[i] = -1;
-                a[j] = -1;
-                break;
-            }
+            if(arr[j] == unique[i])
+                freq[i]++;
         }
     }
 
-    for(int i = 0; i < n; i++)
-        if(a[i] != -1)
-            printf("%d ", a[i]);
+
+    for(int i = 0; i < index; i++)
+    {
+        if(freq[i] == 1)
+            printf("%d ", unique[i]);
+    }
 
     return 0;
 }
