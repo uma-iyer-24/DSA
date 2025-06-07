6db96f7af91e0c4ac5ef1b560bf3692c849d6b90
Uma Iyer <152464173+uma-iyer-24@users.noreply.github.com>
2025-06-07 14:36:58 +0530

    Create MaxAltitude.c

---
+++ SmartInterviews (Hive)/Basic/MaxAltitude.c
@@ -0,0 +1,54 @@
+/*
+Max Altitude bookmark_borderImagine a pilot starting the flight from the ground and flying over a series of different points at different heights. You are given an array, where A[i] represents heights.
+Currently, if the pilot is at altitude X at ith point, and if he wants to reach (i+1)th point, his altitude will become X+A[i].
+The pilot starts at altitude 0 and wants to find the highest point he can reach during the entire journey. Your task is to print the highest altitude the pilot reaches.
+
+Input Format
+The first line of input contains an integer N. The second line of input contains N space-separated integers.
+
+Output Format
+Print the highest altitude the pilot can reach.
+
+Constraints
+1 <= N <= 1000
+-1000 <= A[i] <= 1000
+
+Example
+Input
+5
+-5 1 5 0 -7
+
+Output
+1
+
+Explanation
+
+When the pilot started at point 0 his altitude was -5, when he moved  to point 1 his altitude became (-5 + 1 = -4), at point 2 his altitude became(-4 + 5 = 1), at point 3 
+his became altitude remains(1 + 0 = 1), and at point 4 his altitude became (1 + -7 = -6). The maximum altitude that he reached in his journey was 1.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n, alt = 0, max = 0;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    
+    //fining final altitude and max height
+    for(int i = 0; i < n; i++)
+    {
+        alt += arr[i];
+
+        if(alt > max)
+            max = alt;
+    }
+
+    //output max height
+    printf("%d", max);
+
+    return 0;
+}
