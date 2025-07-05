7d1692f4a50b49bba9838e41e2457789a79b9cce
Uma Iyer <152464173+uma-iyer-24@users.noreply.github.com>
2025-07-05 07:00:16 +0530

    Update LongestContiguous1's.c

--- SmartInterviews (Hive)/Basic/LongestContiguous1's.c
+++ SmartInterviews (Hive)/Basic/LongestContiguous1's.c
@@ -1,5 +1,6 @@
 /*
-Longest Contiguous 1's bookmark_borderGiven an array of elements containing 0's and 1's. You have to find the length of longest contiguous 1's.
+Longest Contiguous 1's 
+Given an array of elements containing 0's and 1's. You have to find the length of longest contiguous 1's.
 
 Input Format
 First line of input contains N - size of the array. The next line contains the N integers of array A.
