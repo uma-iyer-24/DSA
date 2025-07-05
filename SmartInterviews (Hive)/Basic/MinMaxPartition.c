fc2c9f8c9f8f0f7ce80d1150e3dd03abdc3f2fda
Uma Iyer <152464173+uma-iyer-24@users.noreply.github.com>
2025-07-05 07:03:48 +0530

    Update MinMaxPartition.c

--- SmartInterviews (Hive)/Basic/MinMaxPartition.c
+++ SmartInterviews (Hive)/Basic/MinMaxPartition.c
@@ -1,5 +1,34 @@
 /*
+Max Min Partition 
+Given an array D of positive integers, your goal is to divide D into two 
+separate arrays, E and F, under the following conditions:Each element in array D must belong to either array E 
+or array FBoth arrays E and F are non-emptyThe objective is to minimize the partition's value, calculated as the 
+absolute difference between the maximum value of array E (denoted as max(E)) and the minimum value of 
+array F (denoted as min(F))Print the resulting integer that represents the value of this partition.
 
+Input Format
+The first line of input contains N. The second line of input contains an array of size N.
+
+Output Format
+Print the minimized partition value.
+
+Constraints
+2 ≤ N ≤ 104
+1 ≤ Di ≤ 109
+
+Example
+Input
+6
+7 1 14 16 30 4
+
+Output
+2
+
+Explanation
+
+We can partition the array D into E = [7, 1, 14, 4] and F = [16, 30].
+The maximum element of the array E is equal to 14.The minimum element of the array F is equal to 16.The value of the partition 
+is |14 - 16| = 2. It can be proven that 2 is the minimum value out of all the partitions.
 */
 
 #include <stdio.h>
@@ -15,7 +44,6 @@ int main() {
     for(int i = 0; i < n; i++)
         scanf("%d", &d[i]);
 
-    ///////////
     min = abs(d[0] - d[1]);
     for(int i = 0; i < n - 1; i++)
     {
@@ -27,41 +55,6 @@ int main() {
             min = diff;
         }
     }
-    ////////////
-    /*
-    //find min difference
-    min = abs(d[1] - d[0]);
-    for(int i = 0; i < n - 1; i++)
-    {
-        //creating e and f
-        int e[i + 1], f[n - i - 1];
-        for(int j = 0; j <= i; j++)
-            e[j] = d[j];
-        for(int k = i + 1; k < n; k++)
-            f[k - i - 1] = d[k];
-        
-        //finding largest value in e
-        int largest = e[0];
-        for(int j = 0; j <= i; j++)
-            {
-                if(e[j] > largest)
-                    largest = e[j];
-            }
-        
-        //finding smallest value in f
-        int smallest = f[0];
-        for(int k = i + 1; k < n; k++)
-            {
-                if(f[k] < smallest)
-                    smallest = f[k];
-            }
-        
-        //finding difference and checking for min
-        diff = abs(largest - smallest);
-        if(diff < min)
-            min = diff;
-    }
-    */
 
     //output min difference
     printf("%d", min);
