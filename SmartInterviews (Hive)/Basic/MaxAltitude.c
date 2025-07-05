5c2af8d8719d5f5b2dc1d87ac67e133ab440faa2
Uma Iyer <152464173+uma-iyer-24@users.noreply.github.com>
2025-07-05 07:01:19 +0530

    Update MaxAltitude.c

--- SmartInterviews (Hive)/Basic/MaxAltitude.c
+++ SmartInterviews (Hive)/Basic/MaxAltitude.c
@@ -1,5 +1,6 @@
 /*
-Max Altitude bookmark_borderImagine a pilot starting the flight from the ground and flying over a series of different points at different heights. You are given an array, where A[i] represents heights.
+Max Altitude 
+Imagine a pilot starting the flight from the ground and flying over a series of different points at different heights. You are given an array, where A[i] represents heights.
 Currently, if the pilot is at altitude X at ith point, and if he wants to reach (i+1)th point, his altitude will become X+A[i].
 The pilot starts at altitude 0 and wants to find the highest point he can reach during the entire journey. Your task is to print the highest altitude the pilot reaches.
 
