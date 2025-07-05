3f3afd8a50327c043d32634f61d158dd771cd04e
Uma Iyer <152464173+uma-iyer-24@users.noreply.github.com>
2025-07-05 06:59:56 +0530

    Update ListOperations.c

--- SmartInterviews (Hive)/Basic/ListOperations.c
+++ SmartInterviews (Hive)/Basic/ListOperations.c
@@ -1,5 +1,6 @@
 /*
-List Operations bookmarkYou are tasked with implementing a program that manipulates an empty list based on a series of commands.
+List Operations 
+You are tasked with implementing a program that manipulates an empty list based on a series of commands.
 
 Input Format
 The first line of input contains an integer N, indicating the number of commands to follow. The next N lines contains any of the following commands:append X: Appends the integer X to the end of the list.count X: Count the number of occurrences of the integer X in the list.reverse: Reverses the order of elements in the list.insert Pos X: Inserts the integer X at the position Pos in the list.sort: Sorts the elements of the list in ascending order.index X: Gives the index of the first occurrence of the integer X in the list, or -1 if X is not found.length: Gives the length of the list.extend: Extends the list by appending it's current elements to itself.Output Format
